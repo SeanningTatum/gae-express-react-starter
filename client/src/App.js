a347432d65cfa1a4d4ce6c86838984a90e1b30b9
Sean Urgel <seantheurgel@gmail.com>
2019-03-19 23:59:32 +0800

    add-react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,11 @@
 import React from 'react'
+import {Route, BrowserRouter as Router} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route to="/" />
+    </Router>
   )
 }
 
---
+++ client/src/pages/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <>
+      <h5>Landing Page</h5>
+    </>
+  )
+}
+
+export default LandingPage
